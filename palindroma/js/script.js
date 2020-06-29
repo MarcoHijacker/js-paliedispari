4a81d0e1f396c19ba0fad1496aa7aec36de67380
Marco <marco.longo@hotmail.it>
2020-06-29 17:32:36 +0200

    Esercizio Palindoma completo (con funzione).

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,7 +1,7 @@
 // Esercizio Palindroma JS
 
 // Dichiarazione variabili
-var parola = prompt("Inserisci una parola per sapere se è palindroma: ");
+var parola = prompt("Inserisci una parola per controllare se è palindroma: ");
 
 if (isPalindroma(parola)) {
   alert("La parola inserita è palindroma!");
@@ -9,20 +9,15 @@ if (isPalindroma(parola)) {
   alert("La parola inserita NON è palindroma!");
 }
 
-// Definizione funzione
+// Definizione funzioni
 function isPalindroma(word) {
-  var verify = []; // Array in cui salvo i confronti, lettera per lettera
-  for (var i = 0; i < Math.floor(word.length*0.5); i++) { // Se la parola è lunga n, mi bastano n/2 confronti
-    if (word[i] == word[(word.length-1) - i]) { // Verifico, al variare di i da 0 a n/2, se la prima lettera della stringa è uguale all'ultima e così via a 'stringere'
-      verify.push(true); // Se è vero, inserisco un true nell'array verify
-    } else {
-      verify.push(false); // Altrimenti inserisco un falso nell'array verify
-    }
+  var reverseWord = ""; // Stringa
+  for (var i = word.length - 1; i >= 0; i--) { // Inserisco nella stringa reverseWord i caratteri della parola fornita, ma da fondo a cima
+    reverseWord += word[i]; // Associazione caratteri
   }
-  for (var i = 0; i < Math.floor(word.length*0.5); i++) { // Verifico com'è configurato l'array coi confronti lettera per lettera
-    if(verify[i] == false) { // Mi basta avere un false (quindi 2 lettere specchiate diverse) e la parola è subito NON palindroma
-      return false;
-    }
+  if (word == reverseWord) { // Se la parola nell'argomento è uguale a sé stessa, invertita, questa è palindroma
+    return true; // parola palindroma
+  } else {
+    return false; // parola non palindroma
   }
-  return true; // Se arrivo qui, non ho avuto alcun false, quindi la parola sarà palindroma
 }
